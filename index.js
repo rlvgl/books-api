ac4bfbd9f73f25885df64bffb2bf10d9d3883003
Sohum Agrawal <sohum@Sohums-Air.attlocal.net>
2023-02-12 12:57:12 -0800

    fixing port issues during production

--- index.js
+++ index.js
@@ -31,8 +31,4 @@ app.get('/', (req, res) => {
 
 let PORT = process.env.PORT || 8000;
 
-if (process.env.NODE_ENV === 'production') {
-	PORT = `0.0.0.0:$${process.env.PORT}`;
-}
-
 app.listen(PORT, console.log(`Server has started on port ${PORT}`));
